39ce9f88fa5d435c729ec2bbb40d33612bcbc220
Cooxy <procooxy@gmail.com>
2023-06-03 19:31:21 +0200

    New

---
+++ ui.py
@@ -0,0 +1,92 @@
+import tkinter as tk
+import threading
+
+class BlackjackBotApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Blackjack Bot")
+
+        # Variables pour la configuration du bot
+        self.mise_de_base = tk.StringVar()
+        self.multiplicateur = tk.StringVar()
+        self.limite_croupier = tk.StringVar()
+        self.limite_solde = tk.StringVar()
+
+        # Variables pour les statistiques
+        self.nombre_parties = tk.StringVar()
+        self.mise_actuelle = tk.StringVar()
+        self.gain_perte = tk.StringVar()
+
+
+
+        # Champs de saisie pour la configuration du bot
+        self.make_entry("Mise de base :", self.mise_de_base)
+        self.make_entry("Multiplicateur en cas de défaite :", self.multiplicateur)
+        self.make_entry("Limite de pioche : croupier :", self.limite_croupier)
+        self.make_entry("Arret Automatique a partir de :", self.limite_solde)
+
+        # Labels pour les statistiques
+        self.make_label("Nombre de parties :", self.nombre_parties)
+        self.make_label("Mise actuelle :", self.mise_actuelle)
+        self.make_label("Gain/Perte :", self.gain_perte)
+        
+        # Bouton pour démarrer/arrêter le bot
+        self.bot_on = False
+        self.bot_button = tk.Button(root, text="Démarrer", command=self.toggle_bot)
+        self.bot_button.pack()
+        # Bot
+        self.bot_thread = None
+
+    # Créer un champ de saisie avec un label
+    def make_entry(self, text, var):
+        frame = tk.Frame(self.root)
+        label = tk.Label(frame, text=text)
+        label.pack(side=tk.LEFT)
+        entry = tk.Entry(frame, textvariable=var)
+        entry.pack(side=tk.LEFT)
+        frame.pack()
+
+    # Créer un label avec une variable
+    def make_label(self, text, var):
+        frame = tk.Frame(self.root)
+        label = tk.Label(frame, text=text)
+        label.pack(side=tk.LEFT)
+        value_label = tk.Label(frame, textvariable=var)
+        value_label.pack(side=tk.LEFT)
+        frame.pack()
+
+    # Démarrer ou arrêter le bot
+    def toggle_bot(self):
+        self.bot_on = not self.bot_on
+        if self.bot_on:
+            self.bot_button.config(text="Arrêter")
+            # Démarrer le bot dans un nouveau thread
+            self.bot_thread = threading.Thread(target=self.run_bot)
+            self.bot_thread.start()
+        else:
+            self.bot_button.config(text="Démarrer")
+            # Interrompre le bot (vous devrez gérer cela dans la fonction run_bot)
+            self.stop_bot()
+
+    # Fonction pour exécuter le bot
+    def run_bot(self):
+                # Ici, vous pouvez mettre le code pour exécuter le bot
+        # N'oubliez pas de vérifier régulièrement self.bot_on pour savoir si le bot doit s'arrêter
+        # Et mettez à jour les statistiques avec les nouvelles valeurs
+        while self.bot_on:
+            # Mettez ici le code pour une partie de Blackjack
+            # Par exemple :
+            # jouer_partie()
+            # self.nombre_parties.set(int(self.nombre_parties.get()) + 1)
+            pass
+
+    # Fonction pour arrêter le bot
+    def stop_bot(self):
+        # Ici, vous pouvez mettre le code pour arrêter le bot
+        # Par exemple, vous pourriez utiliser une variable de condition pour signaler au bot qu'il doit s'arrêter
+        pass
+
+# Créer l'application
+root = tk.Tk()
+app = BlackjackBotApp(root)
+root.mainloop()
